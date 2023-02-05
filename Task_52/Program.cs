b9f153dab4d37cbaf6c5506559817d693d0bb7e4
alexey.rotar <ar_mail@inbox.ru>
2023-02-05 21:49:49 +0500

    v3

--- Task_52/Program.cs
+++ Task_52/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
-// Генерация случайного 2D-массива размерностью [2...10] и диапазоном значений [0...10]
+// Генерация случайного 2D-массива размерностью [2...10] и диапазоном значений [0...9]
 int[,] CreatingArray2D()
 {
     Random rnd = new Random();
@@ -8,7 +8,7 @@ int[,] CreatingArray2D()
     for (int i=0; i<array.GetLength(0); i++)
     {
         for (int j=0; j<array.GetLength(1); j++)
-            array[i, j] = rnd.Next(0, 11);
+            array[i, j] = rnd.Next(0, 10);
     }
     return array;
 }
@@ -18,9 +18,9 @@ void OutputingArray(float[] array)
 {
     for (int i=0; i<array.Length; i++)
         if (i<array.Length-1) 
-            Console.Write(array[i].ToString("#.0") + "; ");
+            Console.Write(array[i].ToString("0.0") + "; ");
         else
-            Console.Write(array[i].ToString("#.0") + ".");
+            Console.Write(array[i].ToString("0.0") + ".");
             Console.WriteLine("");
 }
 
