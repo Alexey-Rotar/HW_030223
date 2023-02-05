c247a6202249cf0fc4ebc53083e09d8bdb626ac3
alexey.rotar <ar_mail@inbox.ru>
2023-02-06 00:51:21 +0500

    v5

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -20,7 +20,7 @@ void OutputingArray2D(float[,] array)
     for (int i=0; i<array.GetLength(0); i++)
     {
         for (int j=0; j<array.GetLength(1); j++)
-            Console.Write(array[i, j].ToString("0.0") + "  ");
+            Console.Write(array[i, j].ToString("0.0") + "\t");
         Console.WriteLine();
     }
 }
--- Task_52/Program.cs
+++ Task_52/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
+/*
 // Генерация случайного 2D-массива размерностью [2...10] и диапазоном значений [0...9]
 int[,] GeneratingArray2D()
 {
@@ -12,13 +13,32 @@ int[,] GeneratingArray2D()
     }
     return array;
 }
+*/
+
+// Заполнение массива указанной размерности
+int[,] CreatingArray2D(uint lines, uint columns)
+{
+    Console.WriteLine("Вводите построчно значения 2D-массива через пробел:");
+    string input;
+    string[] numberStrings;
+    int[,] array=new int[lines, columns];
+
+    for (int i=0; i<lines; i++)
+    {
+        input=Console.ReadLine()!;
+        numberStrings=input.Split(" ");
+        for (int j=0; j<columns; j++)
+            array[i, j]=int.Parse(numberStrings[j]); 
+    }  
+    return array;
+}
 
 // Вывод массива
 void OutputingArray(float[] array)
 {
     for (int i=0; i<array.Length; i++)
         if (i<array.Length-1) 
-            Console.Write(array[i].ToString("0.0") + "; ");
+            Console.Write(array[i].ToString("0.0") + ";\t");
         else
             Console.Write(array[i].ToString("0.0") + ".");
             Console.WriteLine("");
@@ -31,7 +51,7 @@ void OutputingArray2D(int[,] array)
     for (int i=0; i<array.GetLength(0); i++)
     {
         for (int j=0; j<array.GetLength(1); j++)
-            Console.Write(array[i, j] + "  ");
+            Console.Write(array[i, j] + "\t");
         Console.WriteLine();
     }
 }
@@ -49,8 +69,23 @@ float[] GettingColumnsAverageValues(int[,] array)
     return resultArray;   
 }
 
+// Интерфейс
+Console.WriteLine("Введите количество строк 2D-массива:");
+uint lines;
+while (!uint.TryParse(Console.ReadLine(), out lines) || lines<2)
+{
+    Console.WriteLine("Некорректное значение!");            
+}
 
-int [,] array2D = GeneratingArray2D();
+Console.WriteLine("Введите количество столбцов 2D-массива:");
+uint columns;
+while (!uint.TryParse(Console.ReadLine(), out columns) || columns<2)
+{
+    Console.WriteLine("Некорректное значение!");            
+}
+
+
+int [,] array2D = CreatingArray2D(lines, columns);
 OutputingArray2D(array2D);
 
 Console.WriteLine("Средние значения по столбцам:");
